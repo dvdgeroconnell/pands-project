0d4918eae9b64cf0802efba6b7224b9052641231
David O'Connell <dvdgeroconnell@gmail.com>
2024-04-06 22:22:59 +0100

    Commit initial draft of iris file

--- README.md
+++ README.md
@@ -5,18 +5,23 @@ Programming &amp; Scripting project repo
 This repository contains the files associated with the Programming and Scripting project, the subject of which is an analysis of Fisher's iris data set.
 
 ### Fisher's iris data set
-"The Iris flower dataset is a classic dataset in the field of machine learning and statistical analysis. It consists of 150 observations of iris flowers, including the sepal and petal length and width for each flower, as well as the species of the flower. The dataset was introduced by British statistician and biologist Ronald Fisher in his 1936 paper, *The use of multiple measurements in taxonomic problems*.” See Ref [2].
+"The Iris flower dataset is a classic dataset in the field of machine learning and statistical analysis. It was introduced by British statistician and biologist Ronald Fisher in his 1936 paper, *The use of multiple measurements in taxonomic problems*.” See Ref [2].
+The dataset contains 150 sets of measurements, consisting of 3 sets of 50 measurements across each of 3 species of iris flowers - setosa, versicolor and virginica. The measurements cover the sepal and petal length and width for each flower.
 
 This summary was derived from various sources, notably [1], [2]
 More information may be found in reference 4.
+
+## Source
+The raw dataset in useable csv format was downloaded from [here](https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv). 
 
 
 Note to self - review this - https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-readmes
 
 
 ## References
-xx
+1. [iris dataset wikipedia page](https://en.wikipedia.org/wiki/Iris_flower_data_set)
 2. 
 3.
+
 ****
 #### End
--- iris01.py
+++ iris01.py
@@ -1,10 +1,76 @@
 
+
+# References (additional to README)
+# - https://pandas.pydata.org/docs/getting_started/intro_tutorials/03_subset_data.html#min-tut-03-subset
+
+# ******************************************************************************************************
+
+# Import Pandas for working with the raw dataset
 import pandas as pd
+
+# Import NumPy for working with data arrays 
 import numpy as np
+
+# Import pyplot from matplotlib and seaborn for visualization
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# The iris dataset is available in a csv file here. We will use a local copy.
+# The iris dataset is available in a csv file at this link - we will use a local copy
 # https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv
 
-df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
+# Read the csv file into a Pandas dataframe
+iris = pd.read_csv("iris.csv")
+print(iris.head())
+#print(iris.describe())
+
+# Create a dataframe for each species
+setosa = iris.loc[iris['species']=="setosa", "sepal_length":"petal_width"]
+versicolor = iris.loc[iris['species']=="versicolor", "sepal_length":"petal_width"]
+virginica = iris.loc[iris['species']=="virginica", "sepal_length":"petal_width"]
+# Extract the petal length
+setosa_plen = setosa['petal_length']
+versicolor_plen = versicolor['petal_length']
+virginica_plen = virginica['petal_length']
+
+#plt.plot(setosa_plen, label="setosa", color='red')
+#plt.plot(versicolor_plen, label="versicolor", color='blue')
+#plt.plot(virginica_plen, label="virginica", color='green')
+
+# Create a figure with 3 Axes (subplots), one for each variety 
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(12,4))
+
+# Histogram for setosa
+ax1.hist(setosa_plen, label="setosa", color='red')
+ax1.legend()
+ax1.set_xlabel('Petal Length (cm)')
+ax1.set_ylabel('Number')
+#ax1.set_title('Petal Lengths')
+
+# Histogram for versicolor
+ax2.hist(versicolor_plen, label="versicolor", color='blue')
+ax2.legend()
+ax2.set_xlabel('Petal Length (cm)')
+ax2.set_ylabel('Number')
+#ax2.set_title('Petal Lengths')
+
+# Histogram for virginica
+ax3.hist(virginica_plen, label="virginica", color='green')
+ax3.legend()
+ax3.set_xlabel('Petal Length (cm)')
+ax3.set_ylabel('Number')
+#ax3.set_title('Petal Lengths')
+
+# Set the overall title for the figure
+fig.suptitle("Petal Lengths\n", fontsize=14, fontweight='bold')
+
+# Show the plot
+plt.show()
+
+
+#Get the NumPy array... no need for this
+#num_plen = plen.to_numpy()
+#plt.plot(num_plen)
+#plt.title("Lengths", fontweight='bold')
+#plt.xlabel('Petal Length (cm)')
+#plt.ylabel('Number')
+#plt.legend()
